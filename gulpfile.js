eeef60e6b1c6c971797f27b812fed57cbca3961b
ImBobby <squidz.bee@gmail.com>
2014-07-16 12:56:32 +0700

    replace gulp clean with rimraf #8

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var gulp            = require('gulp'),
     changed         = require('gulp-changed'),
     watch           = require('gulp-watch'),
     rename          = require('gulp-rename'),
-    clean           = require('gulp-clean');
+    rimraf          = require('rimraf');
 
 
 var paths = {
@@ -204,10 +204,8 @@ gulp.task('fonts', function () {
 /* Task: Clean
 --------------------------------------------------------------------------------- */
 
-gulp.task('clean', function () {
-    return gulp
-        .src(paths.build, {read: false})
-        .pipe(clean());    
+gulp.task('clean', function (cb) {
+    rimraf(paths.build, cb);
 });
 
 
