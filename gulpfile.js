ffb0929fe3555551d77cf06b54889f79605177d4
ImBobby <squidz.bee@gmail.com>
2018-05-21 14:22:08 +0700

    Drop IE9 support
    close #150

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ const sassTask = (isMinified = false) => () => {
     const outputStyle = isMinified ? 'compressed' : 'expanded'
     const options = { outputStyle }
     const autoprefixOpts = {
-        browsers: ['> 1%', 'last 10 versions', 'Firefox ESR', 'Opera 12.1']
+        browsers: ['last 2 versions']
     }
 
     return gulp.src(`${paths.dev}sass/main.scss`)
